4986c4463aa6b35193e35230e7f1faf3cf1b1afc
hardikp24 <hardik.patil2494@gmail.com>
2016-10-18 11:51:52 +0530

    resovolved user session id issue

--- login.php
+++ login.php
@@ -8,8 +8,8 @@ if (isset($_SESSION['user_id'])) {
                 <form action='includes/logout.inc.php'>
                     <button>LOG OUT</button>
                 </form><br>
-                <a href='#''>Profile</a>
-                <a href='#'>Setting</a>
+                <a href='#''>Profile</a><br>
+                <a href='#'>Setting</a><br>
             </div>";   
 } 
 
@@ -17,7 +17,7 @@ else {
     
     echo "<aside>
     <div class='widget'>
-        <h2>Login Here</h2>
+        <h2>already have an account? Login Here</h2>
         <div class='inner'>
             <form action='includes/login.inc.php' method='POST'>
                 <input type='text' name='username' placeholder='username'>
@@ -25,12 +25,14 @@ else {
                 <button type='submit'>Login</button>
             </form>
         </div>
+    </div>
+    <div class='widget'>
+            <h2>New here ? </h2>   
+            <h2><a href='signup.php'>Register</a> here to get exlusive offers</h2>
     </div>";
 
 }
 ?>
-<div class="widget">
-            <h2><a href="signup.php">Register</a></h2>
-</div>
+
 
 </aside>
